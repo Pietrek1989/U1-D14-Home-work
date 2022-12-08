bb3eaf4e67ea7dfc90946388bea1d462a17be607
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2022-12-08 16:46:32 +0100

    Homework

---
+++ script.js
@@ -0,0 +1,33 @@
+function randomize() {
+  let randomResult = Math.floor(Math.random(1) * 76 + 1);
+  let classBoard = document.querySelectorAll(".board");
+  let selectedElements = classBoard[randomResult];
+  let randomizedColor = (selectedElements.style.background = randomColor());
+
+  if (selectedElements.classList.contains("selected")) {
+    selectedElements[randomResult + 1].classList.add("selected");
+    selectedElements.style.background = randomColor();
+    randomizedColor;
+  } else {
+    selectedElements.classList.add("selected");
+    selectedElements.style.background = randomColor();
+    randomizedColor;
+  }
+}
+
+function onLoadActions() {
+  for (let number = 1; number <= 76; number++) {
+    let numberNode = document.createElement("div");
+    numberNode.innerText = number;
+    numberNode.classList.add("board", "first-board");
+    document.getElementById("container").appendChild(numberNode);
+  }
+}
+
+function randomColor() {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  randomizedcolor = "#" + randomColor;
+  console.log(randomizedcolor);
+  return randomizedcolor;
+}
+window.onload = onLoadActions;
